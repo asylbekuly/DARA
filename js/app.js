4801e95d544c2f88ef4982b185a7319a5925439c
Ayan <anarbajaan1@gmail.com>
2024-12-23 18:11:38 +0500

    q

--- js/app.js
+++ js/app.js
@@ -1,33 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-// DB connection
-
-const url = 'mongodb+srv://admin:admin@cluster0.qad2i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
-const client = new MongoClient(url, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
-async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
 
 // Middleware для обработки данных формы
 app.use(express.urlencoded({ extended: true }));
@@ -44,17 +17,8 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
     console.log('POST /login сработал!');
     console.log('Данные формы:', req.body);
-    res.redirect('/dashboard');
+    res.redirect('/main');
 });
-
-app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'dashboard.html'));
-});
-
-app.post('/main', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'main.html'));
-});
-
 
 // Маршрут для основной страницы
 app.get('/main', (req, res) => {
@@ -70,7 +34,7 @@ app.get('/forgot', (req, res) => {
 app.post('/forgot-password', (req, res) => {
     const { email } = req.body; // Получаем email из формы
     console.log(`Запрос на восстановление пароля для email: ${email}`);
-
+    
     // После обработки перенаправляем на reset.html
     res.redirect('/reset.html');
 });
@@ -84,7 +48,7 @@ app.get('/reset.html', (req, res) => {
 app.post('/reset-password', (req, res) => {
     const { newPassword } = req.body; // Получаем новый пароль из формы
     console.log(`Новый пароль установлен: ${newPassword}`);
-
+    
     // После обработки перенаправляем на index.html
     res.redirect('/');
 });
